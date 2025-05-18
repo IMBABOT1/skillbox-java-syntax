3366386839463253c415714b21eeb7ea39f8eabd
unknown <zlovred12@gmail.com>
2025-05-18 19:32:07 +0300

    temp

---
+++ src/ru/skillbox/Main.java
@@ -0,0 +1,14 @@
+package ru.skillbox;
+
+import java.time.LocalTime;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println(Math.random());
+        System.out.println(Math.random());
+        System.out.println(Math.random());
+        System.out.println(LocalTime.now());
+
+    }
+}
