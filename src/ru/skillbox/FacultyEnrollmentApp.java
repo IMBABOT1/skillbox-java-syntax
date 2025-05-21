1e14a5da29e3c99d34830e129cc34ceb6736f47e
unknown <zlovred12@gmail.com>
2025-05-21 15:05:20 +0300

    temp

--- src/ru/skillbox/FacultyEnrollmentApp.java
+++ src/ru/skillbox/FacultyEnrollmentApp.java
@@ -1,5 +1,6 @@
 package ru.skillbox;
 
+
 import java.util.Scanner;
 
 public class FacultyEnrollmentApp {
@@ -8,57 +9,21 @@ public class FacultyEnrollmentApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите ваше имя: ");
-        String name = scanner.nextLine();
-        System.out.println("Добро пожаловать: " + name);
+        char answer1;
+        char answer2;
+        char answer3;
 
 
-        System.out.println("Введите количество балллов по математике (0-100): ");
-        int mathScore = scanner.nextInt();
+        System.out.println("Вы согласны, что программирование - это круто? [Y/n]");
+        answer1 = scanner.nextLine().charAt(0);
+        System.out.println("Будете изучать программирование дальше? [Y/n]");
+        answer2 = scanner.nextLine().charAt(0);
+        System.out.println("Собираетесь изменить свою жизнь? [Y/n]");
+        answer3 = scanner.nextLine().charAt(0);
 
 
-        System.out.println("Введите количество балллов по физике (0-100): ");
-        int physicsScore = scanner.nextInt();
-
-        System.out.println("Введите количество балллов по русскому языку (0-100): ");
-        int russianScore = scanner.nextInt();
-
-        System.out.println("Какой минимальный проходной балл? (0-5) ");
-        double minGpa = scanner.nextDouble();
-
-        double scoreToGradePointRatio = 0.05;
-        double mathGradePoint = mathScore * scoreToGradePointRatio;
-        System.out.println("Оценка по математике: " + mathGradePoint);
-
-
-        double physicsGradePoint = physicsScore * scoreToGradePointRatio;
-        System.out.println("Оценка по математике: " + physicsGradePoint);
-
-        double russianGradePoint = russianScore * scoreToGradePointRatio;
-        System.out.println("Оценка по русскому: " + russianGradePoint);
-
-        double gpa = (mathGradePoint + physicsGradePoint + russianGradePoint) / 32;
-        System.out.println("Ваш средний балл: " + gpa);
-
-        boolean hasPassedMinGpa = gpa > minGpa;
-
-        String passMessage = hasPassedMinGpa
-                ? name + ", Вы набрали минимальный проходной балл"
-                : name + ", Вы не набрали минимальный проходной балл";
-
-        System.out.println(name + ", " + passMessage);
-
-        if (hasPassedMinGpa) {
-            if (gpa >= 4.5) {
-                System.out.println("Вы проходите на факультет физики и математики");
-            } else if (gpa >= 4.0) {
-                System.out.println("Вы проходите на приборостроительый факультет");
-            } else if (gpa >= 3.5)
-                System.out.println("Вы проходите на факультет Русского языка");
-            else {
-                System.out.println("Вы проходите на факультет философии");
-            }
-        }
-
+        System.out.println(answer1);
+        System.out.println(answer2);
+        System.out.println(answer3);
     }
 }
